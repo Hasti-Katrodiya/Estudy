a65154c9ba531bc878c2695f0c54bcd879df9341
Hasti-Katrodiya <hastikatrodiya1907@gmail.com>
2023-09-06 17:23:03 +0530

    6/9/23

---
+++ index.php
@@ -0,0 +1,108 @@
+<?php
+include('./includes/header.html');
+?>
+
+<div id="carouselExampleIndicators" class="carousel slide">
+    <div id="carouselExampleIndicators" class="carousel slide">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="./images/slide1.jpg" class="d-block w-100" alt="slide1" height="800px">
+                <div class="overlay">
+                    <div class="content">
+                        <p class="letter_spacing ">FEED YOUR KNOWLEDGE</p>
+                        <h2 class="display-3 bold_font">BEST EDUCATION </h2>
+                        <h2 class="display-3 bold_font">WORDPRESS THEM</h2>
+
+                        <button class="btn btn-warning text-light py-2 px-5 btn_carusal bold_font">Ready to Get
+                            Started</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <section class="section_bg">
+        <div class="container my-5 div_section">
+            <div class="row py-5">
+                <div class="col-md-4 col-lg-4 col-sm-12 ">
+                    <div class="card div_hover p-3 ">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-md-3">
+                                    <i class="fa-solid fa-list-check display-4 py-3 px-2 icon_color"></i>
+                                </div>
+                                <div class="col-md-9">
+                                    <h2 class="bold_font">Online Courses</h2>
+                                    <p class="p_card">Enjoy a Variety of fresh copies</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 col-lg-4 col-sm-12 ">
+                    <div class="card div_hover p-3">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-md-3">
+                                    <i class="fa-regular fa-file display-4 py-3 px-2 icon_color"></i>
+                                </div>
+                                <div class="col-md-9">
+                                    <h2 class="bold_font">Earn a Certificate</h2>
+                                    <p class="p_card">Enjoy a Variety of fresh copies</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 col-lg-4 col-sm-12 ">
+                    <div class="card div_hover p-3">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-md-3">
+                                    <i class="fa-solid fa-graduation-cap display-4 py-3 px-2 icon_color"></i>
+                                </div>
+                                <div class="col-md-9">
+                                    <h2 class="bold_font">Learn with Fun</h2>
+                                    <p class="p_card">Enjoy a Variety of fresh copies</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <div class="container my-4 py-4">
+        <div class="row px-5">
+            <div class="col-mg-6 col-lg-6 col-sm-12">
+                <h2 class="display-5 bold_font">Welcome to Online</h2>
+                <h2 class="display-5 bold_font">
+                    <span class="border_blue">le</span>
+                    <span class="border_yellow">ar</span>
+                    ning Center
+                </h2>
+                <br>
+                <p class="p_card">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus pariatur omnis
+                    praesentium dignissimos quos et aut deleniti ut nisi ea non nesciunt nostrum voluptatum maiores,
+                    quae porro molestiae unde autem?
+                </p>
+
+            </div>
+            <div class="col-mg-6 col-lg-6 col-sm-12">
+
+            </div>
+        </div>
+    </div>
+
+
+    <br><br><br><br>
+
+    <?php
+    include('./includes/footer.html');
+    ?>
+
+    </body>
+
+    </html>
